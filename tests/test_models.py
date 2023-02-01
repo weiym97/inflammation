bcf78c0b4b6ec48e84230477b089ff92c13e857c
weiym97 <weiym97@163.com>
2023-02-01 23:18:56 +0000

    Add initial test cases for daily_max()

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 from unittest.mock import patch
 
 def test_daily_mean_zeros():
@@ -42,3 +43,28 @@ def test_load_csv(mock_get_data_dir):
 
 # TODO(lesson-automatic) Implement tests for the other statistical functions
 # TODO(lesson-mocking) Implement a unit test for the load_csv function
+
+def test_daily_max():
+    """Test that max function works for an array of positive integers."""
+    from inflammation.models import daily_max
+    test_array = np.array([[4,2,5],[1,6,2],[4,1,9]])
+    npt.assert_array_equal(np.array([4,6,9]),daily_max(test_array))
+
+def test_daily_min_string():
+    """TEst for Type Error when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello','there'],['General','Kenobi']])
+
+@pytest.mark.parametrize(
+    "test,expected",
+    [
+        ([[0,0],[0,0],[0,0]],[0,0]),
+        ([[1,2],[3,4],[5,6]],[3,4]),
+    ]
+)
+def test_daily_mean(test,expected):
+    """Test mean function works for array of zeroes and positive numbers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(np.array(expected),daily_mean(np.array(test)))
